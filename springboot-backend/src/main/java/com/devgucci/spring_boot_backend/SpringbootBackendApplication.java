79df25f3353b6a2fe1ab95d176972741f9c80ff8
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-05-15 09:54:31 +0100

    creation spring boot backend with controller, repository and entity-model employee

---
+++ springboot-backend/src/main/java/com/devgucci/spring_boot_backend/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devgucci.spring_boot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/src/main/java/com/devgucci/spring_boot_backend/model/Employee.java
@@ -0,0 +1,27 @@
+package com.devgucci.spring_boot_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
